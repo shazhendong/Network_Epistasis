5fbac137a4f5a39c32c217fede0cc07b2ed80b3f
Zhendong Sha <17443322+shazhendong@users.noreply.github.com>
2023-04-16 16:46:27 -0400

    Update S3_NetworkFiltering.sh

--- S3_NetworkFiltering.sh
+++ S3_NetworkFiltering.sh
@@ -1,4 +1,4 @@
-# This pipeline filter the network by threshold, produce the edgelist and visualize the network.
+# This pipeline filter the network by threshold, produce the edgelist and determin the network community.
 # Author: Zhendong Sha 2023-04-16
 
 #### Parameters ####
@@ -17,3 +17,23 @@ file_script=edge_filter.py
 home=$(pwd)
 
 #### Main ####
+
+cd $dir_target
+mkdir -p $dir_output
+cd $dir_output
+
+## process rat_xor
+edgelist=$file_rat_xor
+cutoff_p=0.03
+
+# copy the edgelist to the output folder
+cp $home/$dir_input/$edgelist .
+# filter the edgelist by pvalue threshold
+python $home/$dir_script/$file_script $edgelist snp1_name snp2_name adj_pval 0.03 se edge_list.csv
+# rename the edgelist
+mv edge_list.csv $edgelist.pvalue_cutoff_$cutoff_p.mod_se.csv
+# determin the network community
+python $home/$dir_script/$file_script $edgelist.pvalue_cutoff_$cutoff_p.mod_se.csv snp1_name snp2_name adj_pval 0.03 se edge_list.csv
+
+# remove the edgelist
+rm $edgelist
